8dc7a6dfbcd67d6a030cc4709e893b6364536861
KucingKode <fazleadyuta4@gmail.com>
2021-11-22 15:58:27 +0800

    refactore the ghost
    - change x, y params to Vector2
    - free queue after ghost killed

--- enemies/Ghost.gd
+++ enemies/Ghost.gd
@@ -13,52 +13,51 @@ var direction = Vector2.ZERO
 func _ready():
 	hide()
 
-	# spawn(0, 0)
-	# set_target(500 , 500)
-	pass
+	# spawn(Vector2(0, 0))
+	# set_target(Vector2(500 , 500))
 
 func _process(delta):
-	var last_x = position.x
-	var last_y = position.y
-
 	$AnimatedSprite.play()
 	$AnimatedSprite.look_at(target)
+	
+	if (!is_reached):
+		var last = position
+		position += direction * get_speed() * delta
+	
+		clamp_pos()
+	
+		if last.x == position.x && last.y == position.y:
+			# target position reached
+			emit_signal('reached')
+			is_reached = true
 
-	position += direction * get_speed() * delta
-
-	clamp_pos()
-
-	if !is_reached && last_x == position.x && last_y == position.y:
-		# target position reached
-		emit_signal('reached')
-		is_reached = true
-
-func set_target(target_x, target_y):
+func set_target(target_pos: Vector2):
 	# reset ghost target
-	var dist_x = target_x - position.x
-	var dist_y = target_y - position.y
+	var dist_x = target_pos.x - position.x
+	var dist_y = target_pos.y - position.y
 
 	initial_pos = position
-	target = Vector2(target_x, target_y)
+	target = target_pos
 	direction = Vector2(dist_x, dist_y).normalized()
 	is_reached = false
 
-func spawn(x, y):
+func spawn(pos: Vector2):
 	# spawn ghost to specific location
-	set_pos(x, y)
+	set_pos(pos)
 	show()
 
 func kill():
 	hide()
+	queue_free()
 
 # helper functions
 func get_speed():
 	var speed = min_speed + position.distance_to(target)
 	return clamp(speed, min_speed, max_speed)
 
-func set_pos(x, y):
-	position.x = x
-	position.y = y
+func set_pos(pos: Vector2):
+	position.x = pos.x
+	position.y = pos.y
 
 func clamp_pos():
 	if target.x > initial_pos.x:
